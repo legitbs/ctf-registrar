728ed284fa3d42be178b09215856c6fbe6c0cf7f
Vito Genovese <vito@legitbs.net>
2012-12-29 16:05:11 -0500

    initial and i did a bunch of work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CtfRegistrar::Application.config.secret_token = 'ee0b64221f5f650ccad9e7999b0a5ecd8d6ed18962b52c8447beb35397b46a046bae29c83e12630425422019494f8a5271b0e35de9cdf8b6e843097aab3ec1e0'
