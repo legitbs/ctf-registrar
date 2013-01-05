1d948656095b5968aa4c7ce0e537ddb2898e44e2
Vito Genovese <vito@legitbs.net>
2013-01-05 15:29:28 -0500

    test team validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,9 @@
 class Team < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :name, :password_digest, :tag
+  attr_accessible :name, :password, :tag
+
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: true, length: {minimum: 4}
+  validates :tag, presence: true, uniqueness: true, length: {maximum: 4}
 end
--- test/unit/team_test.rb
+++ test/unit/team_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'a Team' do
+    setup do
+      @team = FactoryGirl.build :team
+    end
+
+    should belong_to :user
+    should ensure_length_of :tag
+    should validate_presence_of :tag
+    should validate_presence_of :name
+    should validate_uniqueness_of :tag
+    should validate_presence_of :name
+
+    should 'securely store passwords' do
+      @team.password = 'asdf'
+      @team.save
+
+      assert @team.authenticate 'asdf'
+    end
+  end
 end
