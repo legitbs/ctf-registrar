082cedba4259785e0ab6925c4d9473e65b66776e
Vito Genovese <vito@legitbs.net>
2013-01-05 15:29:52 -0500

    clean up teams controller tests

--- test/functional/teams_controller_test.rb
+++ test/functional/teams_controller_test.rb
@@ -1,49 +1,10 @@
 require 'test_helper'
 
 class TeamsControllerTest < ActionController::TestCase
-  setup do
-    @team = teams(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:teams)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create team" do
-    assert_difference('Team.count') do
-      post :create, team: { name: @team.name, password_digest: @team.password_digest, tag: @team.tag }
+  context 'while logged in' do
+    setup do
+      @team = FactoryGirl.build :team
+      session[:user_id] = @team.user.id
     end
-
-    assert_redirected_to team_path(assigns(:team))
-  end
-
-  test "should show team" do
-    get :show, id: @team
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @team
-    assert_response :success
-  end
-
-  test "should update team" do
-    put :update, id: @team, team: { name: @team.name, password_digest: @team.password_digest, tag: @team.tag }
-    assert_redirected_to team_path(assigns(:team))
-  end
-
-  test "should destroy team" do
-    assert_difference('Team.count', -1) do
-      delete :destroy, id: @team
-    end
-
-    assert_redirected_to teams_path
   end
 end
