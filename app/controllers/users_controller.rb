78d88806a2ef125c3bb5af0d7404b177402c44b1
Vito Genovese <vito@legitbs.net>
2013-01-06 00:44:03 -0500

    Allow logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_filter :require_logged_out
   
   def create
+    if params[:commit] == 'Log In'
+      return try_login
+    end
     @user = User.new params[:user]
     if @user.save
       flash[:success] = "You've registered!"
@@ -12,5 +15,17 @@ class UsersController < ApplicationController
     @password_stash = params[:user][:password]
 
     render action: 'new'
+  end
+
+  private
+  def try_login
+    @user = User.where(username: params[:user][:username]).first
+    unless @user && @user.authenticate(params[:user][:password])
+      flash[:error] = "Couldn't log in."
+      return redirect_to root_path
+    end
+
+    self.current_user = @user
+    return redirect_to dashboard_path
   end
 end
