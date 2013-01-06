2ff04d51ab1f1cc772271143ef47b75d44e694b1
Vito Genovese <vito@legitbs.net>
2013-01-05 21:49:11 -0500

    login requirement helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,21 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  def require_logged_in
+    return true if current_user
+    reset_session
+    flash[:error] = "You're not logged in."
+    redirect_to root_path
+  end
+
+  def require_logged_out
+    return true unless current_user
+    flash[:error] = "You're already logged in."
+    redirect_to dashboard_path
+  end
+
   def current_user
-    @current_user ||= User.find session[:user_id]
+    @current_user ||= User.find session[:user_id] rescue nil
   end
 
   def current_user=(user)
