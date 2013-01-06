983996f1748ae1dcdd4e1a6794c7fe2dee547f30
Vito Genovese <vito@legitbs.net>
2013-01-05 22:45:17 -0500

    start of team controller test

--- test/functional/teams_controller_test.rb
+++ test/functional/teams_controller_test.rb
@@ -3,8 +3,18 @@ require 'test_helper'
 class TeamsControllerTest < ActionController::TestCase
   context 'while logged in' do
     setup do
-      @team = FactoryGirl.build :team
-      session[:user_id] = @team.user.id
+      @user = FactoryGirl.create :user
+      session[:user_id] = @user.id
+    end
+
+    context 'without a team' do
+      context 'GET to show' do
+        setup do
+          get :show
+        end
+
+        should redirect_to('dashboard'){ dashboard_path }
+      end
     end
   end
 end
