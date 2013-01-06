1f216e093927dc9f5e6cca7ac4b16eec74521cbc
Vito Genovese <vito@legitbs.net>
2013-01-06 00:44:33 -0500

    Validations and questions for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,20 @@ class User < ActiveRecord::Base
     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
       message: "doesn't look like an email address"
     })
+  validate :owned_team_and_team_must_match
+
+  def owner?
+    participant? && (owned_team == team)
+  end
+
+  def participant?
+    team && team.is_a?(Team) && team.persisted?
+  end
+  
+  private
+  def owned_team_and_team_must_match
+    return unless owner?
+    return if owned_team == team
+    errors.add :owned_team, "must be the same as the participant team"
+  end
 end
