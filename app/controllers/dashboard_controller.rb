6388f1c074784ff8d4a6e57a061029555991d7bd
Vito Genovese <vito@legitbs.net>
2013-01-06 00:44:14 -0500

    Helpers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,17 @@
 class DashboardController < ApplicationController
   before_filter :require_logged_in
+
+  helper_method :team
+
   def index
     unless current_user.participant?
       @new_team = current_user.build_team
       @new_membership = Membership.new
     end
+  end
+
+  private
+  def team
+    @team ||= current_user.team
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def teaminfo(team)
+    content_tag :div, class: "teaminfo team_#{team.id}" do
+      content_tag(:span, team.name, class: 'teamname') +
+      content_tag(:span, team.tag, class: 'teamtag')
+    end
+  end
 end
