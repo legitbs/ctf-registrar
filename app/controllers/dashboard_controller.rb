85fe8b6bfb8f4d6124e77d04fdef5d40ebac6f95
Vito Genovese <vito@legitbs.net>
2013-01-05 23:30:59 -0500

    render teams on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   before_filter :require_logged_in
   def index
+    unless current_user.participant?
+      @new_team = current_user.build_team
+      @new_membership = Membership.new
+    end
   end
 end
