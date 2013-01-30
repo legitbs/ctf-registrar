d7a82a397f3d0b658fdcadd5b79bf74fabe0b76b
Vito Genovese <vito@legitbs.net>
2013-01-29 22:39:39 -0500

    configure mandrill for outbound email

---
+++ config/initializers/mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+}
+ActionMailer::Base.delivery_method = :smtp
