cd97aeb600b3fc1e214799efc2240a4fc736916c
Vito Genovese <vito@legitbs.net>
2013-02-02 14:05:09 -0500

    add google anal-ytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-60'+'5807-18']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
