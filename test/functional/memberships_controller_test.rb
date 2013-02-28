1241f88059a2629b53b97d4043af4996f7fdd0d2
Vito Genovese <vito@legitbs.net>
2013-02-27 20:01:36 -0500

    Test memberships controller

--- test/functional/memberships_controller_test.rb
+++ test/functional/memberships_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class MembershipsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'when logged in' do
+    setup do
+      @user = FactoryGirl.create :user
+      session[:user_id] = @user.id
+      @team = FactoryGirl.create :team
+    end
+
+    context 'without a team' do
+      context 'POST to create' do
+        setup do
+          post :create, membership: {name: @team.name, password: 'kenny'}
+        end
+
+        should redirect_to('the dashboard'){ dashboard_path }
+        should "put the user on a team" do
+          assert_equal @team, @user.reload.team
+        end
+      end
+    end
+  end
 end
