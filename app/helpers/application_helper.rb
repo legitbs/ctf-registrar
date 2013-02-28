00a2610db50ec7e7c533f398ef27abf870818465
Vito Genovese <vito@legitbs.net>
2013-02-27 20:27:05 -0500

    Remove tag from teaminfo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
   def teaminfo(team)
     content_tag :div, class: "teaminfo team_#{team.id}" do
-      content_tag(:span, team.name, class: 'teamname') +
-      content_tag(:span, team.tag, class: 'teamtag')
+      content_tag(:span, team.name, class: 'teamname')
     end
   end
 end
