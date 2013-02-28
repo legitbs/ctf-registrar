330e6a705d4bcbee0633b094e8cad9d72390fb41
Vito Genovese <vito@legitbs.net>
2013-02-27 20:00:50 -0500

    Membership is by team name

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,13 +2,13 @@ class Membership
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :tag, :password, :team
+  attr_accessor :name, :password, :team
 
   def initialize(attributes = {})
     return if attributes.blank?
 
     @attrs = attributes.stringify_keys
-    self.tag = @attrs['tag']
+    self.name = @attrs['name']
     self.password = @attrs['password']
 
     try_join
@@ -20,7 +20,7 @@ class Membership
 
   private
   def try_join
-    @candidate ||= Team.find_by_tag tag
+    @candidate ||= Team.find_by_name name
     return if @candidate.nil?
 
     return unless @candidate.authenticate password
