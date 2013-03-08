c86c1dc44c71aa9002c6b7a833657639ee420191
Vito Genovese <vito@legitbs.net>
2013-03-08 18:17:14 -0500

    Refactor analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       content_tag(:span, team.name, class: 'teamname')
     end
   end
+
+  def analytics_data
+    accum = []
+    accum += flash[:analytics] unless flash[:analytics].nil?
+    accum += @analytics unless @analytics.nil?
+    accum << ['_setCustomVar', 1, 'Username', request.env['REMOTE_USER']]
+    "_gaq.push(#{accum.to_json});"
+  end
 end
