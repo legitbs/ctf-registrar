bc5c15dd5ce206d71ec7d3a18afca615482c4ff0
Vito Genovese <vito@legitbs.net>
2013-04-24 20:29:54 -0400

    SQL-based scoreboard data loading

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :order
+
+  def self.for_scoreboard
+    connection.select_rows <<-SQL
+      SELECT c.id, c.name
+      FROM categories as c
+      ORDER BY c.id asc
+      SQL
+  end
 end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,25 @@
 class Challenge < ActiveRecord::Base
   belongs_to :category
   attr_accessible :answer_digest, :clue, :name, :points
+
+  def self.for_scoreboard(team)
+    challenge_rows = connection.select_all <<-SQL
+      SELECT
+        a.name as category_name,
+        c.name as challenge_name,
+        c.points,
+        s.created_at
+      FROM
+        challenges AS c
+        INNER JOIN categories AS a
+          ON c.category_id = a.id
+        LEFT JOIN solutions AS s
+          ON c.id = s.challenge_id and s.team_id=#{team.id.to_i}
+      ORDER BY
+        c.points asc,
+        a.id asc
+    SQL
+
+    challenge_rows.group_by{|r| r['points']}.values
+  end
 end
