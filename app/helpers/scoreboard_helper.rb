6c2b8b036bbc7c3ae076aebd3e8c9f2741ef2f83
Vito Genovese <vito@legitbs.net>
2013-05-25 16:54:47 -0400

    add burning class

--- app/helpers/scoreboard_helper.rb
+++ app/helpers/scoreboard_helper.rb
@@ -1,6 +1,11 @@
 module ScoreboardHelper
   def challenge_class_for(single_challenge_row)
-    return '' if single_challenge_row['created_at'].nil?
-    return 'solved'
+    return 'solved' unless single_challenge_row['created_at'].nil?
+    return 'burning' if single_challenge_row['unlocked_at'] && !single_challenge_row['solved_at']
+    return ''
+  end
+
+  def challenge_id_for(single_challenge_row)
+    "#{single_challenge_row['category_name'].parameterize}-#{single_challenge_row['points']}"
   end
 end
