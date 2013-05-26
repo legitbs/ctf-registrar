bc53df1a7adb013fdd6972570053ac2dda980f77
Vito Genovese <vito@legitbs.net>
2013-05-25 23:31:58 -0400

    run the queries

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,6 +1,9 @@
 class ScoreboardController < ApplicationController
   def index
+    @leaderboard = Team.for_scoreboard Team.find(7)
     @categories = Category.for_scoreboard
-    @challenges = Challenge.for_scoreboard(Team.first)
+    @challenges = Challenge.for_scoreboard Team.first
+
+    @challenge = Challenge.find 12
   end
 end
