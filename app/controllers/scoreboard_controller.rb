71d80991ac035de78053d48d8c18d15c9b03eb76
Vito Genovese <vito@legitbs.net>
2013-05-30 21:22:03 -0400

    less janky answering

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -33,21 +33,42 @@ class ScoreboardController < ApplicationController
     @challenge = Challenge.find params[:id]
     @solution = current_team.solution_for @challenge
 
-    return redirect_to scoreboard_path if @challenge.locked?
-    return redirect_to scoreboard_path if @solution.created_at
+    sticky_situation = @challenge.locked? || @solution.created_at
+    correct = @challenge.correct_answer?(params[:answer]) rescue false
 
-    unless @challenge.correct_answer? params[:answer]
-      return redirect_to challenge_path(@challenge.id) 
+    if sticky_situation || !correct
+      return respond_to do |f|
+        f.html { redirect_to challenge_path(@challenge.id) }
+        f.json { render status: 400, json: {
+          error: "Now that's what I call a sticky situation.",
+          wrong: !correct,
+          weird: sticky_situation
+         }}
+      end
     end
 
+    hot = nil
     @solution.transaction do
       hot = @challenge.solve!
       @solution.save
       if hot
         current_team.hot = true
         current_team.save
-        redirect_to 
       end
+    end
+
+    respond_to do |f|
+      f.html {
+        return redirect_to choice_path if hot
+        return redirect_to scoreboard_path
+      }  
+
+      f.json {
+        render status: 200, json: {
+          woot: 'you got it',
+          hot: hot
+        }
+      }
     end
   end
 
