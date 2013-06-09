f7360a209204530a16baa0f6a0bcda0948c68e96
Vito Genovese <vito@legitbs.net>
2013-06-09 17:07:34 -0400

    final category names

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+categories = {}
+
 [
-  "transfer protocol", # web
-  "ethically sourced", # exploitation
-  "Ghoti in the Shell", # shellcode
-  "10100111001", # guerilla programming
+  "3dub", # web
+  "0x41414141", # exploitation
+  "\\xff\\xe4\\xcc", # shellcode
+  "OMGACM", # guerilla programming
   "gnireenigne", # reverse engineering
 ].each_with_index do |name, order|
-  Category.where(name: name, order: order).first_or_create
+  categories[name] = Category.where(name: name, order: order).first_or_create
 end
+
+fart = "$2a$10$/tcBNuCm5oCxpuzfUeq8luZFS/Rk.IoOVsyVzcdSX7.uXxcI4Dlj6"
 
 Category.find_each do |cat|
   (1..5).each do |val|
-    Challenge.where(name: "#{cat.name} #{val}",
+    c = Challenge.where(name: "#{cat.name} #{val}",
                     clue: "#{cat.name}",
                     category_id: cat.id,
                     points: val
                     ).first_or_create
+    c.answer_digest = fart
+    c.save
   end
 end
