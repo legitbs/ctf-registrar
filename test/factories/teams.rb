5bb280e54225232f49f41fac47f8c4e2a1496e67
Vito Genovese <vito@legitbs.net>
2013-09-02 17:59:15 -0400

    password confirmation in factories

--- test/factories/teams.rb
+++ test/factories/teams.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :team do
     name { "butt team #{rand(10000)}" }
     password 'kenny'
+    password_confirmation 'kenny'
     user
   end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :user do
     username { "fart #{rand(1000)}" }
     password "new york"
+    password_confirmation "new york"
     email { "butt#{rand(1000)}@example.com" }
 
     factory :user_params_valid do
