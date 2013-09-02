be0abbcc782d24bc0248e81d3b60156601700cbf
Vito Genovese <vito@legitbs.net>
2013-09-02 17:52:11 -0400

    fix a shoulda error

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,12 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  include Shoulda::Matchers::ActiveRecord
+  extend Shoulda::Matchers::ActiveRecord
+  include Shoulda::Matchers::ActiveModel
+  extend Shoulda::Matchers::ActiveModel
+
   def be_during_game
     @controller.stubs(:during_game?).returns(true)
   end
