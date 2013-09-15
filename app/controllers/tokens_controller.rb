594162a66806ff72d3f1a98a9c6f69176acd9cd7
Vito Genovese <vito@legitbs.net>
2013-09-15 19:58:22 -0400

    working OTP generation

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,7 +1,62 @@
-class TokensController < ApplicationController
+class TokensController < ApplicationController  
+  before_filter :require_logged_in
+  before_filter :require_no_token, only: %i{new create}
+
   def new
+    @temporary_secret = ROTP::Base32.random_base32
+    session[:temporary_secret] = @temporary_secret
+  end
+
+  def create
+    unless params['i-promise']
+      flash[:alert] = "Seriously, you have to promise to turn this on."
+      return redirect_to action: 'new'
+    end
+    
+    if session[:temporary_secret] != params[:temporary_secret]
+      flash[:alert] = "Something got wonky."
+      return redirect_to action: 'new' 
+    end
+    @temporary_secret = session[:temporary_secret]
+
+    @candidate = params['first-code']
+    
+    verifier = ROTP::TOTP.new(@temporary_secret)
+    unless verifier.verify(@candidate.to_i)
+      flash[:alert] = "Failed the secret check, let's try again with a new secret."
+      return redirect_to action: 'new'
+    end
+
+    current_user.auth_secret = @temporary_secret
+    current_user.transaction do
+      @fallback_tokens = 10.times.map do 
+        current_user.fallback_tokens.create.as_token_string
+      end
+      current_user.save
+    end
   end
 
   def edit
+  end
+
+  def show
+    respond_to do |fmt|
+      fmt.png do
+        secret = params[:id]
+        unless secret =~ /^[a-zA-Z0-9]{16}$/
+          return render qrcode: 'lol', status: 400
+        end
+        payload = "otpauth://totp/legitbs%202014?secret=#{secret}"
+        render qrcode: payload
+      end
+    end
+  end
+
+  private
+  def require_no_token
+    return true unless current_user.auth_secret
+    
+    redirect_to dashboard_path
+    return false
   end
 end
--- app/models/fallback_token.rb
+++ app/models/fallback_token.rb
@@ -1,3 +1,33 @@
 class FallbackToken < ActiveRecord::Base
   belongs_to :user
+  
+  before_create :assign_key_and_secret
+  
+  def as_token_string
+    [key, @secret].join('-')
+  end
+  
+  def self.find_by_token_string(token_string)
+    key, secret = token_string.split('-')
+    
+    candidate = find_by key: key
+
+    return nil unless candidate
+
+    return nil unless BCrypt::Password.new(candidate.secret_digest) == secret
+
+    return candidate
+  end
+
+  private
+  def assign_key_and_secret
+    self.key = slug
+    @secret = slug
+
+    self.secret_digest = BCrypt::Password.create @secret
+  end
+
+  def slug
+    SecureRandom.random_number(36**10).to_s(36)
+  end
 end
