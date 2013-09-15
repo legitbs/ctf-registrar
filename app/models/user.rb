2e00860fb1b0e1a968f4405f8315761d8181fa0b
Vito Genovese <vito@legitbs.net>
2013-09-15 15:38:42 -0400

    fix the visa attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   has_one :owned_team, class_name: 'Team'
   attr_accessible :username, :email, :email_confirmation
   attr_accessible :password, :password_confirmation
-  
+  attr_accessible :visa
+
   has_secure_password
 
   validates :username, :email, presence: true, uniqueness: true
