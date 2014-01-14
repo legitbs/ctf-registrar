d1496ba9fe20dbe4b05193a41a4dcb0894c27315
Vito Genovese <vito@legitbs.net>
2014-01-13 21:47:39 -0500

    log less in cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -60,3 +60,5 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+
+Rails.logger.level = 2
