4197ee9a1d4753fdf24bb8abcf6715d861916246
Vito Genovese <vito@legitbs.net>
2014-01-18 16:27:25 -0500

    reset mailer

---
+++ app/mailers/reset_mailer.rb
@@ -0,0 +1,17 @@
+class ResetMailer < ActionMailer::Base
+  default from: "team@legitbs.net", template_path: 'reset_mailer'
+
+  def reset_email(reset)
+    @reset = reset
+    @user = @reset.user
+
+    mail to: user.email, subject: 'Attempting to reset your DEF CON CTF password'
+  end
+
+  def did_reset_email(reset)
+    @reset = reset
+    @user = @reset.user
+
+    mail to: user.email, subject: 'Your DEF CON CTF password was reset'
+  end
+end
