49a13c42d590cfe40f03f8510e91325d5e4b5023
Vito Genovese <vito@legitbs.net>
2014-03-29 16:47:43 -0400

    test the achievement text on the page instead of in the database

--- features/step_definitions/cheevo_steps.rb
+++ features/step_definitions/cheevo_steps.rb
@@ -1,3 +1,4 @@
 Then 'my team should have the "$cheevo_name" achievement' do |cheevo_name|
-  assert @user.reload.team.achievements.where(name: cheevo_name).first
+  assert page.has_content?("Achievement Unlocked!")
+  assert page.has_content?(cheevo_name)
 end
