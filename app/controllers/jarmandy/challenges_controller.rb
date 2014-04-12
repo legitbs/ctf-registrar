e5edeafd7c52863893a98ad16b3ab8d1fe54d8ad
Vito Genovese <vito@legitbs.net>
2014-04-12 18:43:38 -0400

    users and teams views

---
+++ app/controllers/jarmandy/challenges_controller.rb
@@ -0,0 +1,15 @@
+class Jarmandy::ChallengesController < Jarmandy::BaseController
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- app/controllers/jarmandy/teams_controller.rb
+++ app/controllers/jarmandy/teams_controller.rb
@@ -1,4 +1,15 @@
 class Jarmandy::TeamsController < Jarmandy::BaseController
   def index
+    if !params[:q].blank?
+      @source = 'Search results'
+      @teams = Team.search(params[:q]).order(created_at: :desc)
+    else
+      @source = 'New teams'
+      @teams = Team.order(created_at: :desc)
+    end
+  end
+
+  def show
+    @team = Team.find params[:id]
   end
 end
