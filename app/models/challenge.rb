7775def434ce4583ddcd27ad7e4c3c41f8cafe78
Vito Genovese <vito@legitbs.net>
2014-05-16 16:18:38 -0400

    use min_unlock

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -31,13 +31,13 @@ class Challenge < ActiveRecord::Base
   def self.for_picker(team)
     challenges = for_scoreboard team
 
-    category_max_solved = Category.all.inject({  }) do |mem, cat|
-      mem[cat.name] = cat.challenges.where.not(solved_at: nil).map(&:points).max || 1
+    category_min_unsolved = Category.all.inject({  }) do |mem, cat|
+      mem[cat.name] = cat.challenges.where(solved_at: nil).map(&:points).min || 1
       mem
     end
 
     challenges.flatten.each do |c|
-      max_unlock = category_max_solved[c['category_name']] + 1
+      max_unlock = category_min_unsolved[c['category_name']]
       next c['class'] = 'solved' if c['created_at']
       next c['class'] = 'live' if c['unlocked_at']
       next c['class'] = 'locked' if c['points'].to_i > max_unlock
