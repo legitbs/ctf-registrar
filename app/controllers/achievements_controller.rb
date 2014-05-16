1845b700d59e5f7c63e300a3ab45bae235351acd
Vito Genovese <vito@legitbs.net>
2014-05-16 14:14:25 -0400

    show achievements

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,7 +1,19 @@
 class AchievementsController < ApplicationController
   before_filter :require_on_team
+  helper_method :awards
 
   def index
-    @awards = current_team.awards.order(created_at: :asc)
+    @team = current_team
+  end
+
+  def show
+    @team = Team.find(params[:id])
+    render 'index'
+  end
+
+  private
+
+  def awards
+    @awards ||= @team.awards.order(created_at: :asc)
   end
 end
