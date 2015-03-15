4af0566c28cdd56f8d7df0083d582af9e2272be1
Vito Genovese <vito@legitbs.net>
2015-03-15 18:33:43 -0400

    update date and fix totp

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,4 +1,4 @@
-class TokensController < ApplicationController  
+class TokensController < ApplicationController
   before_filter :require_logged_in
   before_filter :require_no_token, only: %i{new create}
 
@@ -12,24 +12,24 @@ class TokensController < ApplicationController
       flash[:alert] = "Seriously, you have to promise to turn this on."
       return redirect_to action: 'new'
     end
-    
+
     if session[:temporary_secret] != params[:temporary_secret]
       flash[:alert] = "Something got wonky."
-      return redirect_to action: 'new' 
+      return redirect_to action: 'new'
     end
     @temporary_secret = session[:temporary_secret]
 
     @candidate = params['first-code']
-    
+
     verifier = ROTP::TOTP.new(@temporary_secret)
-    unless verifier.verify(@candidate.to_i)
+    unless verifier.verify(@candidate)
       flash[:alert] = "Failed the secret check, let's try again with a new secret."
       return redirect_to action: 'new'
     end
 
     current_user.auth_secret = @temporary_secret
     current_user.transaction do
-      @fallback_tokens = 10.times.map do 
+      @fallback_tokens = 10.times.map do
         current_user.fallback_tokens.create.as_token_string
       end
       current_user.save
@@ -43,7 +43,7 @@ class TokensController < ApplicationController
         unless secret =~ /^[a-zA-Z0-9]{16}$/
           return render qrcode: 'lol', status: 400
         end
-        payload = "otpauth://totp/legitbs%202014?secret=#{secret}"
+        payload = "otpauth://totp/legitbs%202015?secret=#{secret}"
         send_data Pngqr.encode(payload, scale: 9), disposition: :inline, type: 'image/png'
       end
     end
@@ -52,7 +52,7 @@ class TokensController < ApplicationController
   private
   def require_no_token
     return true unless current_user.auth_secret
-    
+
     redirect_to dashboard_path
     return false
   end
