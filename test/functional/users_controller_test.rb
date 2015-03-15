e8852115b60ac3bd64db00a7fa471ceb44992323
Vito Genovese <vito@legitbs.net>
2015-03-15 16:27:54 -0400

    we're not doing homepage create

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,17 +2,6 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   context 'the users controller' do
-    should 'show a form on incomplete (home screen) create' do
-      be_before_game
-      @user_attrs = FactoryGirl.attributes_for :user
-      @user_attrs.delete :email
-
-      post :create, user: @user_attrs
-
-      assert_template 'new'
-      assert_select 'form[method=post]'
-    end
-
     context 'on successful create' do
       setup do
         be_before_game
