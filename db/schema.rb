7fb4a400046581d1ecfad75afa04fe6876ff8291
Vito Genovese <vito@legitbs.net>
2015-03-15 13:12:11 -0400

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,8 @@ ActiveRecord::Schema.define(version: 20140504193953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
-  create_table "achievements", force: true do |t|
+  create_table "achievements", force: :cascade do |t|
     t.string   "name"
     t.string   "condition"
     t.string   "description"
@@ -27,9 +26,10 @@ ActiveRecord::Schema.define(version: 20140504193953) do
     t.datetime "updated_at"
   end
 
+  add_index "achievements", ["name"], name: "index_achievements_on_name", using: :btree
   add_index "achievements", ["trophy_id"], name: "index_achievements_on_trophy_id", using: :btree
 
-  create_table "awards", force: true do |t|
+  create_table "awards", force: :cascade do |t|
     t.integer  "achievement_id"
     t.integer  "team_id"
     t.integer  "user_id"
@@ -43,14 +43,14 @@ ActiveRecord::Schema.define(version: 20140504193953) do
   add_index "awards", ["team_id"], name: "index_awards_on_team_id", using: :btree
   add_index "awards", ["user_id"], name: "index_awards_on_user_id", using: :btree
 
-  create_table "categories", force: true do |t|
+  create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.integer  "order"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "challenges", force: true do |t|
+  create_table "challenges", force: :cascade do |t|
     t.string   "name"
     t.string   "clue"
     t.string   "answer_digest"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20140504193953) do
 
   add_index "challenges", ["category_id"], name: "index_challenges_on_category_id", using: :btree
 
-  create_table "fallback_tokens", force: true do |t|
+  create_table "fallback_tokens", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "key"
     t.string   "secret_digest"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 20140504193953) do
 
   add_index "fallback_tokens", ["user_id"], name: "index_fallback_tokens_on_user_id", using: :btree
 
-  create_table "notices", force: true do |t|
+  create_table "notices", force: :cascade do |t|
     t.string   "body"
     t.integer  "team_id"
     t.datetime "created_at"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 20140504193953) do
   add_index "notices", ["team_id", "created_at"], name: "index_notices_on_team_id_and_created_at", using: :btree
   add_index "notices", ["team_id"], name: "index_notices_on_team_id", using: :btree
 
-  create_table "resets", force: true do |t|
+  create_table "resets", force: :cascade do |t|
     t.integer  "user_id",      null: false
     t.string   "key",          null: false
     t.string   "digest",       null: false
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20140504193953) do
   add_index "resets", ["key"], name: "index_resets_on_key", unique: true, using: :btree
   add_index "resets", ["user_id"], name: "index_resets_on_user_id", using: :btree
 
-  create_table "solutions", force: true do |t|
+  create_table "solutions", force: :cascade do |t|
     t.integer  "team_id"
     t.integer  "challenge_id"
     t.datetime "created_at"
@@ -108,10 +108,10 @@ ActiveRecord::Schema.define(version: 20140504193953) do
   end
 
   add_index "solutions", ["challenge_id"], name: "index_solutions_on_challenge_id", using: :btree
-  add_index "solutions", ["team_id", "challenge_id"], name: "index_solutions_on_team_id_and_challenge_id", using: :btree
+  add_index "solutions", ["team_id", "challenge_id"], name: "index_solutions_on_team_id_and_challenge_id", unique: true, using: :btree
   add_index "solutions", ["team_id"], name: "index_solutions_on_team_id", using: :btree
 
-  create_table "teams", force: true do |t|
+  create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
     t.integer  "user_id",         null: false
@@ -125,13 +125,13 @@ ActiveRecord::Schema.define(version: 20140504193953) do
   add_index "teams", ["name"], name: "index_teams_on_name", using: :btree
   add_index "teams", ["user_id"], name: "index_teams_on_user_id", using: :btree
 
-  create_table "trophies", force: true do |t|
+  create_table "trophies", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
     t.integer  "team_id"
@@ -149,18 +149,12 @@ ActiveRecord::Schema.define(version: 20140504193953) do
   add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
   add_foreign_key "achievements", "trophies", name: "achievements_trophy_id_fk"
-
   add_foreign_key "awards", "achievements", name: "awards_achievement_id_fk"
   add_foreign_key "awards", "teams", name: "awards_team_id_fk"
   add_foreign_key "awards", "users", name: "awards_user_id_fk"
-
   add_foreign_key "challenges", "categories", name: "challenges_category_id_fk"
-
   add_foreign_key "solutions", "challenges", name: "solutions_challenge_id_fk"
   add_foreign_key "solutions", "teams", name: "solutions_team_id_fk"
-
   add_foreign_key "teams", "users", name: "teams_user_id_fk"
-
   add_foreign_key "users", "teams", name: "users_team_id_fk"
-
 end
