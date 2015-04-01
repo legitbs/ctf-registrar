b7c5cddd8df284c7c6ef2718b881303b2d87e8d0
Vito Genovese <vito@legitbs.net>
2015-03-31 23:42:44 -0400

    get raygun token from env

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -1,7 +1,9 @@
-Raygun.setup do |config|
-  config.api_key = "SEhLvs4oqiHFn5Ku0SoU/g=="
-  config.filter_parameters = Rails.application.config.filter_parameters
+if defined? Raygun
+  Raygun.setup do |config|
+    config.api_key = ENV['RAYGUN_APIKEY']
+    config.filter_parameters = Rails.application.config.filter_parameters
 
-  # The default is Rails.env.production?
-  # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+    # The default is Rails.env.production?
+    # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+  end
 end
