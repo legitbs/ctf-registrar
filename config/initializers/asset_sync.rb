14a055b80e07ec268ab2eab050ffe8646ffbbd70
Vito Genovese <vito@legitbs.net>
2015-04-01 00:57:06 -0400

    update asset config

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -3,7 +3,7 @@ begin
     config.fog_provider = 'AWS'
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    config.fog_directory = 'assets-2014.legitbs.net'
+    config.fog_directory = 'assets-2015.legitbs.net'
 
     Fog.credentials = { path_style: true }
 
@@ -14,9 +14,9 @@ begin
     # config.existing_remote_files = "keep"
     #
     # Automatically replace files with their equivalent gzip compressed version
-    # config.gzip_compression = true
+    config.gzip_compression = true
     #
-    # Use the Rails generated 'manifest.yml' file to produce the list of files to 
+    # Use the Rails generated 'manifest.yml' file to produce the list of files to
     # upload instead of searching the assets directory.
     # config.manifest = true
     #
