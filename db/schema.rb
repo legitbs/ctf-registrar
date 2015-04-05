7e87f0e5032b5c54a19658ee05b2bed1b40c57b7
Vito Genovese <vito@legitbs.net>
2015-04-05 16:49:20 -0400

    migrate team logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140504193953) do
+ActiveRecord::Schema.define(version: 20150405204455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,12 +114,17 @@ ActiveRecord::Schema.define(version: 20140504193953) do
   create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
-    t.integer  "user_id",         null: false
+    t.integer  "user_id",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "hot"
     t.boolean  "prequalified"
     t.boolean  "fun"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
+    t.string   "logo_fingerprint"
   end
 
   add_index "teams", ["name"], name: "index_teams_on_name", using: :btree
