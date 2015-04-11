335f809693ebb65493a0c98ba40ec693ab743305
Vito Genovese <vito@legitbs.net>
2015-04-11 11:20:57 -0400

    slim down a long line

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,10 @@ class Team < ActiveRecord::Base
 
   has_secure_password
 
-  validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximum: 60}
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: {minimum: 3, maximum: 60}
   validates :user, presence: true
 
   has_many :solutions
