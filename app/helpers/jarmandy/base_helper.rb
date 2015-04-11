a27649965db42d09446bc2df93c84ff335be15b6
Vito Genovese <vito@legitbs.net>
2015-04-11 11:18:09 -0400

    fix pagination on all the pages

--- app/helpers/jarmandy/base_helper.rb
+++ app/helpers/jarmandy/base_helper.rb
@@ -5,8 +5,8 @@ module Jarmandy::BaseHelper
     @pagination_controls ||= content_tag :ul, class: 'pagination' do
       @pages.times.map do |n|
         content_tag :li do
-          options = {page: n + 1, only: params[:only]}
-          options.delete :page if options[:page] == 1
+          options = {page: (n + 1).to_s, only: params[:only]}
+          options.delete :page if options[:page] == '1' && @current_page == 1
           link_to_unless_current(n + 1, params.merge(options))
         end
       end.join.html_safe
