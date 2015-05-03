b76ce7e6a1c742bdd3bfdb17b9744bf3da417f16
Vito Genovese <vito@legitbs.net>
2015-05-03 15:18:56 -0400

    pg stat statements module

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150405204455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "achievements", force: :cascade do |t|
     t.string   "name"
