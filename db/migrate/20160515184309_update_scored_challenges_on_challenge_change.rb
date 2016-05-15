53ee9ac3413b894475daeeb437036b7d7e68f214
Vito Genovese <vito@legitbs.net>
2016-05-15 14:46:32 -0400

    update scored challenges on challenge change

---
+++ db/migrate/20160515184309_update_scored_challenges_on_challenge_change.rb
@@ -0,0 +1,10 @@
+class UpdateScoredChallengesOnChallengeChange < ActiveRecord::Migration
+  def change
+    Solution.connection.execute <<-SQL
+      CREATE TRIGGER scored_challenges_update_on_challenge_change
+        AFTER INSERT OR UPDATE ON solutions
+        FOR EACH STATEMENT
+        EXECUTE PROCEDURE scored_challenges_refresh_proc();
+    SQL
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512174805) do
+ActiveRecord::Schema.define(version: 20160515184309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
