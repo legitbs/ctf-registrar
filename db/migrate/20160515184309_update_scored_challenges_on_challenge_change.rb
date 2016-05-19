a53689a6a973cbc0327748f3a6c769282aa3c1de
Vito Genovese <vito@legitbs.net>
2016-05-18 21:01:42 -0400

    fix a migration

--- db/migrate/20160515184309_update_scored_challenges_on_challenge_change.rb
+++ db/migrate/20160515184309_update_scored_challenges_on_challenge_change.rb
@@ -2,7 +2,7 @@ class UpdateScoredChallengesOnChallengeChange < ActiveRecord::Migration
   def change
     Solution.connection.execute <<-SQL
       CREATE TRIGGER scored_challenges_update_on_challenge_change
-        AFTER INSERT OR UPDATE ON solutions
+        AFTER INSERT OR UPDATE ON challenges
         FOR EACH STATEMENT
         EXECUTE PROCEDURE scored_challenges_refresh_proc();
     SQL
